2684e2185b929f7a8b42167c48f9a453f9d63991
Andrew Thomas <andrew.bower.thomas@gmail.com>
2020-06-16 20:02:55 -0500

    Ticker scroll now continuous.

--- DGWEB/DGReactApp/src/components/Ticker.js
+++ DGWEB/DGReactApp/src/components/Ticker.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from 'react';
+import { CSSTransition } from "react-transition-group";
 import './css/Ticker.css';
 
 export class Ticker extends Component {
@@ -11,7 +12,7 @@ export class Ticker extends Component {
 
 	componentDidMount() {
 		this.updateQuotes();
-		this.interval = setInterval(() => { this.updateQuotes(); }, 1000);
+		this.interval = setInterval(() => { this.updateQuotes(); }, 3000);
 	}
 
 	componentWillUnmount() {
@@ -21,24 +22,39 @@ export class Ticker extends Component {
 	async updateQuotes() {
 		const response = await fetch('api/quotes');
 		var data = await response.json();
-		data.sort(function (a, b) {
-			return a.symbol.localeCompare(b.symbol);
-		});
+		if (data.length > 0) {
+			data.sort(function (a, b) {
+				return a.symbol.localeCompare(b.symbol);
+			});
+        }
 		this.setState({ quotes: data });
 	}
 
 	render() {
+		const quotesContainer = (
+			<div className="ticker-scroll">
+				{this.state.quotes.map((quote) => (
+					<div key={quote.symbol} className="ticker-item">
+						{quote.symbol}: <TickerPrice price={quote.price} />
+					</div>
+				))}
+			</div>);
 		return (
 			<div className="" id="ticker">
+				{/* https://reneroth.org/marquees-in-css/ */}
 				<div className="ticker-wrap">
-					<div className="ticker">
+					{quotesContainer}
+					{quotesContainer}
+				</div>
+				{/*<div className="ticker-wrap">
+					<div className="ticker ticker1">
 						{this.state.quotes.map((quote) => (
 							<div key={quote.symbol} className="ticker-item">
 								{quote.symbol}: <TickerPrice price={quote.price} />
 							</div>
 						))}
 					</div>
-				</div>
+				</div>*/}
 			</div>
 		);
 	}
